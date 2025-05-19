95e0b76f0f934e8aeee4c6b25568974f8117dd2b
WesitosFsa <137083225+WesitosFsa@users.noreply.github.com>
2025-05-19 13:06:24 -0500

    feat(maps): Añadiendo mbtiles en assets

--- lib/core/config.dart
+++ lib/core/config.dart
@@ -3,7 +3,7 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 class SupabaseConfig {
   static const String supabaseUrl = 'URL';
   static const String supabaseAnonKey = 'KEY';
-  
+
   static Future<void> init() async {
     await Supabase.initialize(
       url: supabaseUrl,
